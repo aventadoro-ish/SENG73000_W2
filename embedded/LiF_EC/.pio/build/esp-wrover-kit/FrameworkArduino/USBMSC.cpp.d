a2aa01bce932d9c87fb008910b3fcd49b56c3d13
Matvey <aventadoroqwerty@gmail.com>
2026-06-13 23:31:35 -0400

    added logbook entries and accidentally added Arduino code for EC

---
+++ embedded/LiF_EC/.pio/build/esp-wrover-kit/FrameworkArduino/USBMSC.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/esp-wrover-kit/FrameworkArduino/USBMSC.cpp.o: \
+ C:/Users/Matve/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.cpp \
+ C:/Users/Matve/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
+ C:/Users/Matve/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
