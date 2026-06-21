3ceda0cc2b4e4d8a4dab1e12f29e68d2bb887164
Matvey <aventadoroqwerty@gmail.com>
2026-06-21 14:01:02 -0400

    copied RPi code to LiF_SC

---
+++ RPi/LiF_SC/include/mainFunctions.h
@@ -0,0 +1,34 @@
+#ifndef MAIN_FUNCTIONS
+#define MAIN_FUNCTIONS
+
+// Queue 
+#define QUEUE_SIZE 3
+
+extern int requestQueue[QUEUE_SIZE];
+extern int queueHead;
+extern int queueTail;
+extern int queueCount;
+
+void enqueueFloor(int floor);
+int dequeueFloor();
+
+// Main menu
+int menu();
+int chooseID();
+int chooseMsg();
+int HexFromFloor(int floorVal);
+int FloorFromHex(int Hex);
+
+// States
+typedef enum {
+    STATE_IDLE,        // 0 - no message request, check DB and CAN for requests
+    STATE_MOVING,      // 1 - message to EC, cart is moving and waiting for ARRIVED target floor
+    STATE_ARRIVED,     // 2 - EC reached it's target floor, updating DB, setting to IDLE
+    STATE_FAULT        // 3 - Before timeout exists
+} State;
+
+extern State currentState;
+
+#define MOVE_TIMEOUT_SEC 15
+
+#endif
---
+++ RPi/LiF_SC/src/main.cpp
@@ -0,0 +1,185 @@
+#include "../include/pcanFunctions.h"
+#include "../include/databaseFunctions.h"
+#include "../include/mainFunctions.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h> 
+#include <iostream>
+#include <time.h> // for timeout
+
+using namespace std;
+State currentState = STATE_IDLE;
+
+
+// ******************************************************************
+
+int main() {
+
+	int choice; 
+	int ID; 
+	int data; 
+	int numRx;
+	int floorNumber = 1, prev_floorNumber = 1;
+
+	while(1) {
+		system("@cls||clear");
+		choice = menu(); 
+		switch (choice) {
+			case 1: 
+				ID = chooseID();		// user to select ID depending on intended recipient
+				data = chooseMsg();		// user to select message data
+				pcanTx(ID, data);		// transmit ID and data 
+				db_setFloorNum(FloorFromHex(data)); 		// change floor number in database ** NEW **
+				break; 
+				
+			case 2:
+				printf("\nHow many messages to receive? ");
+				scanf("%d", &numRx);
+				pcanRx(numRx);
+				break;
+				
+			case 3:
+				printf("\nNow listening to commands from the website - press ctrl-z to cancel\n");
+				
+				pcanRxInit();
+	
+				// Synchronize elevator db and CAN (start at 1st floor)
+				pcanTx(ID_SC_TO_EC, GO_TO_FLOOR1);
+				db_setFloorNum(1);
+				currentState = STATE_IDLE;
+
+				{
+					TPCANMsg incoming;
+					int targetFloor = 1;
+					//int activeTargetFloor = -1;
+					time_t moveStartTime = 0;
+
+					while(1){			
+
+						//Check if there is a new message request, flag
+						int pendingFloor = -1; //no other message in the queue
+						bool newRequest = false;
+
+						//Check database for requests
+
+						floorNumber = db_getFloorNum();
+						if (prev_floorNumber != floorNumber) {								// If floor number changes in database
+							pendingFloor = floorNumber;
+							enqueueFloor(floorNumber);
+						}
+
+						prev_floorNumber = floorNumber; 
+
+						// Check CAN for floor requests
+						int gotMessage = pcanRxState(&incoming);
+						if (gotMessage)
+						{
+							switch(incoming.ID) {
+								case ID_F1_TO_SC:
+									enqueueFloor(1);
+									break;
+								case ID_F2_TO_SC:
+									enqueueFloor(2);
+									break;
+								case ID_F3_TO_SC:
+									enqueueFloor(3);
+									break;
+								case ID_CC_TO_SC:
+									// CC_FloorReq is bits 1-0 of the data byte
+									enqueueFloor(incoming.DATA[0] & 0x03);
+									break;
+								case ID_EC_TO_ALL:
+						
+									break;
+							}
+						}
+
+						//FSM Logic
+						switch(currentState) {
+
+						case STATE_IDLE:
+							if (queueCount > 0) {
+								targetFloor = dequeueFloor();
+								pcanTx(ID_SC_TO_EC, HexFromFloor(targetFloor));
+								moveStartTime = time(NULL);
+								printf("Moving to floor %d\n", targetFloor);
+								currentState = STATE_MOVING;
+							}
+						
+							break;
+
+						case STATE_MOVING:
+	
+							if ((time(NULL) - moveStartTime)
+								> MOVE_TIMEOUT_SEC)
+							{
+								printf("ERROR: Elevator timeout\n");
+
+								currentState = STATE_FAULT;
+								break;
+							}
+
+							if (gotMessage &&
+								incoming.ID == ID_EC_TO_ALL)
+							{
+								int reportedFloor =
+									incoming.DATA[0] & 0x03;
+
+								if (reportedFloor == targetFloor)
+								{
+									currentState =
+										STATE_ARRIVED;
+								}
+							}
+
+							break;
+					
+
+						case STATE_ARRIVED:
+							db_setFloorNum(targetFloor);
+							currentState = STATE_IDLE;
+							break;
+
+						// Error handler
+						case STATE_FAULT:
+
+							printf("\nFAULT STATE\n");
+							printf("Elevator did not arrive within timeout\n");
+
+							while(queueCount > 0)
+							{
+								dequeueFloor();
+							}
+
+							sleep(3);
+
+							currentState = STATE_IDLE;
+
+							break;
+						}
+					}
+					pcanRxClose();
+				}
+				break;
+				
+			case 4: 
+				return(0);
+			
+			default:
+				printf("Error on input values");
+				sleep(3);
+				break;
+		}
+		sleep(1);					// delay between send/receive
+	}
+	
+	return(0);
+}
+
+
+
+
+
+
+	
