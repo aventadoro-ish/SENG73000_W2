e46bdb978e71dc33033e6f87d7a291c90cca96d2
YevtushenkoRita <ryevtushenko2993@conestogac.on.ca>
2026-07-27 12:19:53 -0400

    fixed typos

--- RPi/LiF_SC/src/main.cpp
+++ RPi/LiF_SC/src/main.cpp
@@ -127,12 +127,12 @@ int main() {
 	}
 
 
-	if (!database.db_connect) {
+	if (!database.db_connect()) {
 		return -1;
 	}
 	
 	// Initializes the request reader and ignores existing requests.
-	db.read_floor_request();
+	database.read_floor_request();
 
 
 
